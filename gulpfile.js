6ecc7f82ca91378530ec6001c8e618d7d5f0d91e
Joaby <joabyoliveira2004@gmail.com>
2021-07-22 10:17:54 -0300

    build: set up gulp and start project

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const autoPrefixer = require('gulp-autoprefixer')
+const browserSync = require('browser-sync').create()
+
+function compilaSass() {
+  return gulp
+    .src('styles/css/scss/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoPrefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('styles/css/'))
+    .pipe(browserSync.stream())
+}
+gulp.task('sass', compilaSass)
+
+function browser() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  })
+}
+
+gulp.task('browser-sync', browser)
+
+function watch() {
+  gulp.watch('styles/css/scss/*.scss', compilaSass)
+}
+
+gulp.task('watch', watch)
+gulp.task('default', gulp.parallel(['watch', 'browser-sync']))
